d74852beb1a1fb74200aaec6b68e134ddca42fd3
nanlei <36439285+nanlei2000@users.noreply.github.com>
2019-11-14 15:08:17 +0800

    feat: 添加`stringify`

--- src/parser.ts
+++ src/parser.ts
@@ -1,114 +1,114 @@
-export function parser(str: string): any {
-  let i = 0;
+export function parse(str: string): any {
+    let i = 0;
 
-  function parseValue(): any {
-    if (isNumberChar(str[i])) {
-      return parseNumber();
+    function parseValue(): any {
+        if (isNumberChar(str[i])) {
+            return parseNumber();
+        }
+        const currentChar = str[i] as '{' | '[' | 'n' | 't' | 'f' | '"';
+        switch (currentChar) {
+            case '{':
+                return parseObject();
+            case '[':
+                return parseArray();
+            case 'n':
+                return parseBoolAndNull('null');
+            case 't':
+                return parseBoolAndNull('true');
+            case 'f':
+                return parseBoolAndNull('false');
+            case '"':
+                return parseString();
+            default:
+                assertNever(currentChar);
+        }
     }
-    const currentChar = str[i] as '{' | '[' | 'n' | 't' | 'f' | '"';
-    switch (currentChar) {
-      case '{':
-        return parseObject();
-      case '[':
-        return parseArray();
-      case 'n':
-        return parseBoolAndNull('null');
-      case 't':
-        return parseBoolAndNull('true');
-      case 'f':
-        return parseBoolAndNull('false');
-      case '"':
-        return parseString();
-      default:
-        assertNever(currentChar);
-    }
-  }
 
-  function parseString(): string {
-    let res = '';
-    i++;
-    while (str[i] !== '"') {
-      res += str[i++];
-    }
-    i++;
-    return res;
-  }
-
-  const strValueMap = {
-    null: null,
-    true: true,
-    false: false,
-  };
-
-  function parseBoolAndNull(
-    expectedStr: keyof typeof strValueMap
-  ): boolean | null {
-    const res = str.substr(i, expectedStr.length);
-    if (res === expectedStr) {
-      i += expectedStr.length;
-      return strValueMap[expectedStr];
-    } else {
-      throw new Error('Unexpected char at pos: ' + i);
-    }
-  }
-
-  function parseNumber(): number {
-    let numStr = '';
-    while (isNumberChar(str[i])) {
-      numStr += str[i++];
-    }
-    return parseFloat(numStr);
-  }
-
-  function isNumberChar(char: string): boolean {
-    return '1234567890-+eE.'.includes(char);
-  }
-
-  function parseArray(): any[] {
-    i++;
-    const res: any[] = [];
-    while (str[i] !== ']') {
-      res.push(parseValue());
-      if (str[i] === ',') {
+    function parseString(): string {
+        let res = '';
         i++;
-      }
-    }
-    i++;
-    return res;
-  }
-
-  function parseObject(): { [key: string]: any } {
-    i++;
-    const res: { [key: string]: any } = {};
-    while (str[i] !== '}') {
-      const key = parseString();
-      i++;
-      const value = parseValue();
-      res[key] = value;
-      if (str[i] === ',') {
+        while (str[i] !== '"') {
+            res += str[i++];
+        }
         i++;
-      }
+        return res;
     }
-    i++;
-    return res;
-  }
 
-  function assertNever(_value: never): never {
-    throw new Error('Unexpected char at pos: ' + i);
-  }
+    const strValueMap = {
+        null: null,
+        true: true,
+        false: false,
+    };
 
-  return parseValue();
+    function parseBoolAndNull(
+        expectedStr: keyof typeof strValueMap
+    ): boolean | null {
+        const res = str.substr(i, expectedStr.length);
+        if (res === expectedStr) {
+            i += expectedStr.length;
+            return strValueMap[expectedStr];
+        } else {
+            throw new Error('Unexpected char at pos: ' + i);
+        }
+    }
+
+    function parseNumber(): number | bigint {
+        let numStr = '';
+        while (isNumberChar(str[i])) {
+            numStr += str[i++];
+        }
+        const intOrFloat = parseFloat(numStr);
+        if (Number.isInteger(intOrFloat) && !Number.isSafeInteger(intOrFloat)) {
+            return BigInt(numStr)
+        } else {
+            return intOrFloat;
+        }
+    }
+
+    function isNumberChar(char: string): boolean {
+        return '1234567890-+eE.'.includes(char);
+    }
+
+    function parseArray(): any[] {
+        i++;
+        const res: any[] = [];
+        while (str[i] !== ']') {
+            res.push(parseValue());
+            if (str[i] === ',') {
+                i++;
+            }
+        }
+        i++;
+        return res;
+    }
+
+    function parseObject(): { [key: string]: any } {
+        i++;
+        const res: { [key: string]: any } = {};
+        while (str[i] !== '}') {
+            const key = parseString();
+            i++;
+            const value = parseValue();
+            res[key] = value;
+            if (str[i] === ',') {
+                i++;
+            }
+        }
+        i++;
+        return res;
+    }
+
+    function assertNever(_value: never): never {
+        throw new Error('Unexpected char at pos: ' + i);
+    }
+
+    return parseValue();
 }
 
 // test
-const str = JSON.stringify({
-  a: 0.1,
-  b: false,
-  c: true,
-  d: null,
-  e: 'hello',
-  f: [1, 2, { a: 1, b: false, c: true, d: null, e: 'hello' }],
-});
-const res = parser(str);
-console.log('→: res', JSON.stringify(res));
-console.log('→: res', JSON.stringify(res) === str);
+// const str = `{"a":0.1, "b": false, "c": true, "d": null, "e": "hello", "f": [123456789876543210, 2, {"a": 1, "b": false, "c": true, "d": null, "e": "你好"}]}`;
+const str = '123456789876543210';
+const res = parse(str);
+console.log('→: res', res);
+console.log('→: res', typeof res);
+// console.log('→: res', JSON.stringify(res) === str);
---
+++ src/stringify.ts
@@ -0,0 +1,60 @@
+/** 复制于:https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON */
+function stringify(value: any): any {
+    const toString = Object.prototype.toString;
+    const isArray = Array.isArray
+        || function (a) { return toString.call(a) === '[object Array]'; };
+    const escMap = {
+        '"': '\\"',
+        '\\': '\\\\',
+        '\b': '\\b',
+        '\f': '\\f',
+        '\n': '\\n',
+        '\r': '\\r',
+        '\t': '\\t'
+    };
+    const escFunc = function (m: keyof typeof escMap) {
+        return escMap[m] ||
+            '\\u' + (m.charCodeAt(0) + 0x10000).toString(16).substr(1);
+    };
+    const escRE = /[\\"\u0000-\u001F\u2028\u2029]/g;
+    function _stringify(value: any): any {
+        if (value == null) {
+            return 'null';
+            // 添加了这两行
+        } else if (typeof value === 'bigint') {
+            return value.toString();
+        } else if (typeof value === 'number') {
+            return isFinite(value) ? value.toString() : 'null';
+        } else if (typeof value === 'boolean') {
+            return value.toString();
+        } else if (typeof value === 'object') {
+            if (typeof value.toJSON === 'function') {
+                return _stringify(value.toJSON());
+            } else if (isArray(value)) {
+                var res = '[';
+                for (var i = 0; i < value.length; i++)
+                    res += (i ? ', ' : '') + _stringify(value[i]);
+                return res + ']';
+            } else if (toString.call(value) === '[object Object]') {
+                var tmp = [];
+                for (var k in value) {
+                    if (value.hasOwnProperty(k))
+                        tmp.push(_stringify(k) + ': ' + _stringify(value[k]));
+                }
+                return '{' + tmp.join(', ') + '}';
+            }
+        }
+        return '"' + value.toString().replace(escRE, escFunc) + '"';
+    };
+
+    return _stringify(value)
+}
+
+console.log(stringify({
+    a: 0.1,
+    b: false,
+    c: true,
+    d: null,
+    e: 'hello',
+    f: [123456789876543210n, 2, { a: 1, b: false, c: true, d: null, e: '你好' }],
+}))
